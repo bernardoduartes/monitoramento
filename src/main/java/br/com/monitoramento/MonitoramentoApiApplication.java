27ded2628a666690cebfd1ff3a22196f09ab4c21
Bernardo Duarte <bernardo.silva@infobase.com.br>
2022-06-15 14:09:14 -0300

    first commit

---
+++ src/main/java/br/com/monitoramento/MonitoramentoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.monitoramento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/monitoramento/MonitoramentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.monitoramento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
